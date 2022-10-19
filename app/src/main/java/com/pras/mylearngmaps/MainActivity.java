2210f59e74bccb38ac2ba8b835604338ac5189a2
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-19 21:30:30 +0700

    Add google maps view

--- app/src/main/java/com/pras/mylearngmaps/MainActivity.java
+++ app/src/main/java/com/pras/mylearngmaps/MainActivity.java
@@ -1,14 +1,39 @@
 package com.pras.mylearngmaps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+
+        // Add a marker on my home and move the camera
+        LatLng myHome = new LatLng(-8.0122842, 112.6274347);
+        gMap.addMarker(new MarkerOptions()
+                .position(myHome)
+                .title("Marker on myHome"));
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(myHome));
     }
 }
