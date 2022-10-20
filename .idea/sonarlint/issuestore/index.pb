90d673999338e7f57d70fdc71b2c423aebfe3cb9
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-20 23:05:08 +0700

    Add get user current location

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+X
+(gradle/wrapper/gradle-wrapper.properties,f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+>
+app/.gitignore,5/1/51e1c5d383dfaa35e0e7e5873a0a99355a86880f
+{
+Kapp/src/androidTest/java/com/pras/mylearngmaps/ExampleInstrumentedTest.java,8/2/82951a733485b4477455a8661df89fe038bd1a96
+F
+app/proguard-rules.pro,9/e/9e08934d811afe28fbc77aaa3c0d747b94348db9
+l
+<app/src/test/java/com/pras/mylearngmaps/ExampleUnitTest.java,c/8/c816a594d46d3d7a831a230f949341dbeefeeef7
--- app/src/main/java/com/pras/mylearngmaps/MainActivity.java
+++ app/src/main/java/com/pras/mylearngmaps/MainActivity.java
@@ -1,15 +1,19 @@
 package com.pras.mylearngmaps;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -18,7 +22,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap gMap;
     private RadioGroup rgTopLocation;
     private LatLng locStiki, locAlunAlun, locMuseumBrawijaya, locMatos, locMog;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locMuseumBrawijaya = new LatLng(-7.9719815, 112.6208468);
         locMatos = new LatLng(-7.9568425000000005, 112.61874174035411);
         locMog = new LatLng(-7.9769603, 112.6238642);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -53,23 +61,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     break;
             }
         });
+
+        fusedLocationProviderClient
+                .getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                        gMap.addMarker(new MarkerOptions()
+                                        .position(currentLoc)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                                        .title("User Current Location"))
+                                .showInfoWindow();
+                        moveCamera(currentLoc);
+                    } else {
+                        moveCamera(locStiki);
+                    }
+                });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         gMap = googleMap;
 
-        // Add a marker on my home and move the camera
-        LatLng myHome = new LatLng(-8.0122842, 112.6274347);
         gMap.addMarker(new MarkerOptions().position(locStiki));
         gMap.addMarker(new MarkerOptions().position(locAlunAlun));
         gMap.addMarker(new MarkerOptions().position(locMuseumBrawijaya));
         gMap.addMarker(new MarkerOptions().position(locMatos));
         gMap.addMarker(new MarkerOptions().position(locMog));
-        gMap.addMarker(new MarkerOptions()
-                .position(myHome)
-                .title("Marker on myHome"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myHome, 15));
     }
 
     private void moveCamera(LatLng loc) {
